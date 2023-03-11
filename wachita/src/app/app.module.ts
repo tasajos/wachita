3a984eb86edb2376fa49daea0227455758eea33e
Carlos Azcarraga <tasajos@gmail.com>
2023-03-11 17:51:35 -0400

    second stage

--- wachita/src/app/app.module.ts
+++ wachita/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//componentes
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgregarWachitaComponent } from './Components/agregar-wachita/agregar-wachita.component';
 import { VerWachitaComponent } from './Components/ver-wachita/ver-wachita.component';
 import { ListadoWachitaComponent } from './Components/listado-wachita/listado-wachita.component';
 import { PrincipalComponent } from './Components/principal/principal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { PrincipalComponent } from './Components/principal/principal.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
