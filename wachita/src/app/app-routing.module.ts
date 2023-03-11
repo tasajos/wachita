9c0485c20c1612a3d33d0b4aee901c14036b7dc4
Carlos Azcarraga <tasajos@gmail.com>
2023-03-11 17:35:28 -0400

    routerlink

--- wachita/src/app/app-routing.module.ts
+++ wachita/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarWachitaComponent } from './Components/agregar-wachita/agregar-wachita.component';
+import { ListadoWachitaComponent } from './Components/listado-wachita/listado-wachita.component';
+import { PrincipalComponent } from './Components/principal/principal.component';
+import { VerWachitaComponent } from './Components/ver-wachita/ver-wachita.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: '', redirectTo:'principal',pathMatch:'full'},
+  {path: '', redirectTo:'principal',pathMatch:'full'},
+  {path: 'principal', component:PrincipalComponent},
+  {path: 'agregarw', component:AgregarWachitaComponent},
+  {path: 'listarw', component:ListadoWachitaComponent},
+  {path: 'verwachita/:id', component:VerWachitaComponent},
+  //{path: '**', redirectTo: 'principal', pathMatch:'full'},
+  {path: '**', redirectTo: 'principal', pathMatch:'full'},
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
