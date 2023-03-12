2803013f84302dd3483de43cdbad9200ae6a0f86
Carlos Azcarraga <tasajos@gmail.com>
2023-03-12 11:15:18 -0400

    getwachita

--- wachita/src/app/Servicios/ser-wachita.service.ts
+++ wachita/src/app/Servicios/ser-wachita.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SerWachitaService {
 
-  constructor() { }
+  private Myappurl: string = environment.endpoint;
+  private Myapiurl: string = 'api/Wachita/';
+
+  constructor(private http: HttpClient) { }
+
+    getwachita():Observable<any>{
+return this.http.get(`${this.Myappurl}${this.Myapiurl}`);
+
+    }
+  
 }
