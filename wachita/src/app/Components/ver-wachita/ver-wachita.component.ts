3f66fdc43764950871d55552851473121d08da83
Carlos Azcarraga <tasajos@gmail.com>
2023-03-12 18:21:09 -0400

    udpate

--- wachita/src/app/Components/ver-wachita/ver-wachita.component.ts
+++ wachita/src/app/Components/ver-wachita/ver-wachita.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InWachita } from 'src/app/Interfaz/in-wachita';
+import {SerWachitaService} from 'src/app/Servicios/ser-wachita.service';
+
 
 @Component({
   selector: 'app-ver-wachita',
   templateUrl: './ver-wachita.component.html',
   styleUrls: ['./ver-wachita.component.css']
 })
-export class VerWachitaComponent {
+export class VerWachitaComponent  implements OnInit{
 
-}
+  id:number;
+  idWachita!: InWachita;
+ 
+  
+    constructor (private _verwachita:SerWachitaService,
+      private aRoute: ActivatedRoute) {
+  
+        this.id = +this.aRoute.snapshot.paramMap.get('id')!;
+        
+       }
+  
+  
+    ngOnInit(): void {
+      this.obtenerWachita();
+    }
+  obtenerWachita(){
+  //this._verwachita.getwachita(this.id).subscribe(data => this.idWachita =data);
+  this._verwachita.getwachita(this.id).subscribe(data =>this.idWachita =data);
+  }
+  
+  }
